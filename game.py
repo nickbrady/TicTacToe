6caa85139b9553fe1e47830931c86f06706494da
Nick <nicholas.w.brady@gmail.com>
2022-01-09 17:08:21 +0100

    Update game.py
    add data visualization of random game results: bar chart + frequency of moves in won / lost / tied games 
    implemented a more optimal game strategy: choose middle, then corners, then anything else

--- game.py
+++ game.py
@@ -1,7 +1,21 @@
-import numpy as np
+import sys
+sys.path.insert(0, '/Users/nicholasbrady/Documents/School/Academic/West Research/Projects/')
+from PythonHelperFunctions import *
+plot_parameters()
+csfont = {'fontname':'Serif'}
+# latfont = {'Computer Modern Roman'}
+# matplotlib.rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+# matplotlib.rc('text', usetex=False)
+# matplotlib.rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+# matplotlib.rc('text', usetex=True)
+# https://matplotlib.org/users/customizing.html
+matplotlib.rc('xtick', top=True, bottom=True, direction='in')
+matplotlib.rc('ytick', left=True, right=True, direction='in')
+plt.rc("axes.spines", top=True, right=True)
+matplotlib.rc('axes', edgecolor='k')
+
 import random
 import math
-import pandas as pd
 
 '''
     There are 9 game over conditions: 8 game won conditions, 1 board full (tie-game)
@@ -98,8 +112,8 @@ class Board:
         return ([i for i in range(self.board.size)
                 if self.board[i] != CELL_EMPTY])
 
-    # def get_random_valid_move_index(self):                    # don't really want this; prefer random play
-    #     return random.choice(self.get_valid_move_indexes())   # to be done outside of the board
+    # def get_random_valid_move_index(self):                    # don't really want this; prefer play
+    #     return random.choice(self.get_valid_move_indexes())   # strategies to be done outside of board
 
     def print_board(self):
         print(self.get_board_as_string())
@@ -138,7 +152,7 @@ def get_symbol(cell):
     return '-'
 
 # In[2]:
-# investigation of the code
+# investigation of the code and game function
 board = Board()
 
 print(board.board_2d)
@@ -168,7 +182,10 @@ board.print_board()
 
 
 
-# In[200]:
+# In[3]:
+# play xxx number of random games
+# investigate break down of random games
+
 number_games = 10000
 game_results = np.zeros(number_games)
 game_moves   = np.zeros(number_games)
@@ -181,23 +198,21 @@ for i in range (number_games):
     game_results[i] = board.get_game_result()
     game_moves[i]   = np.count_nonzero(board.board)
 
-# In[200]:
-print(np.count_nonzero(game_moves == 5))
-print(np.count_nonzero(game_moves == 6))
-print(np.count_nonzero(game_moves == 7))
-print(np.count_nonzero(game_moves == 8))
-print(np.count_nonzero(game_moves == 9) - np.count_nonzero(game_results == 0))
-print(np.count_nonzero(game_results == 0))
-# np.count_nonzero(board.board)
+# In[4]:
+print('\t \t \t (%)')
+print('Ties: \t \t \t', np.count_nonzero(game_results == 0) / len(game_results) * 100)
+print('X Wins: \t \t', np.count_nonzero(game_results == 1) / len(game_results) * 100)
+print('O Wins: \t \t', np.count_nonzero(game_results == -1) / len(game_results) * 100)
+print()
 
-# In[201]:
-print(game_results[:10])
+print('5 Move Games: \t \t', np.count_nonzero(game_moves == 5) / len(game_results) * 100)
+print('6 Move Games: \t \t', np.count_nonzero(game_moves == 6) / len(game_results) * 100)
+print('7 Move Games: \t \t', np.count_nonzero(game_moves == 7) / len(game_results) * 100)
+print('8 Move Games: \t \t', np.count_nonzero(game_moves == 8) / len(game_results) * 100)
+print('9 Move Games (Wins): \t', (np.count_nonzero(game_moves == 9) - np.count_nonzero(game_results == 0)) / len(game_results) * 100)
+print('9 Move Games (Ties): \t', np.count_nonzero(game_results == 0) / len(game_results) * 100)
 
-print('Ties: \t', np.count_nonzero(game_results == 0))
-print('X Wins: ', np.count_nonzero(game_results == 1))
-print('O Wins: ', np.count_nonzero(game_results == -1))
-
-# In[202]:
+# In[5]:
 # Use sampling with replacement to get mean and standard deviations
 number_resamples = 1000
 ties = np.zeros(number_resamples)
@@ -213,114 +228,18 @@ for i in range(number_resamples):
 ties = ties/len(sub_sample)*100
 X_win = X_win/len(sub_sample)*100
 O_win = O_win/len(sub_sample)*100
-print(np.mean(ties), np.std(ties))
-print(np.mean(X_win), np.std(X_win))
-print(np.mean(O_win), np.std(O_win))
 
-# In[203]:
-# use game board to check all possible games
-
-board_list = []
-
-five_move_games = 0
-six_move_games = 0
-seven_move_games = 0
-eight_move_games = 0
-nine_move_wins = 0
-nine_move_ties = 0
-
-board = Board()
-legal_moves_1 = board.get_valid_move_indexes()
-
-for l1 in legal_moves_1:
-    board = Board()
-    board = board.play_move(l1)
-
-    legal_moves_2 = board.get_valid_move_indexes()
-    for l2 in legal_moves_2:
-        board_state = board.play_move(l2)
-        legal_moves_3 = board_state.get_valid_move_indexes()
-
-        for l3 in legal_moves_3:
-            board_state = board.play_move(l2).play_move(l3)
-            legal_moves_4 = board_state.get_valid_move_indexes()
-
-            for l4 in legal_moves_4:
-                board_state = board.play_move(l2).play_move(l3).play_move(l4)
-                legal_moves_5 = board_state.get_valid_move_indexes()
-
-                for l5 in legal_moves_5:
-                    board_state = board.play_move(l2).play_move(l3).play_move(l4).play_move(l5)
-
-                    if board_state.is_gameover():
-                        board_list.append(board_state.board_2d)
-                        legal_moves_6 = []
-                        five_move_games += 1
-
-                    else:
-                        legal_moves_6 = board_state.get_valid_move_indexes()
-
-                    for l6 in legal_moves_6:
-                        board_state = board.play_move(l2).play_move(l3).play_move(l4).play_move(l5).play_move(l6)
-
-                        if board_state.is_gameover():
-                            board_list.append(board_state.board_2d)
-                            legal_moves_7 = []
-                            six_move_games += 1
-
-                        else:
-                            legal_moves_7 = board_state.get_valid_move_indexes()
-
-                        for l7 in legal_moves_7:
-                            board_state = board.play_move(l2).play_move(l3).play_move(l4).play_move(l5).play_move(l6).play_move(l7)
-
-                            if board_state.is_gameover():
-                                board_list.append(board_state.board_2d)
-                                legal_moves_8 = []
-                                seven_move_games += 1
-
-                            else:
-                                legal_moves_8 = board_state.get_valid_move_indexes()
-
-                            for l8 in legal_moves_8:
-                                board_state = board.play_move(l2).play_move(l3).play_move(l4).play_move(l5).play_move(l6).play_move(l7).play_move(l8)
-
-                                if board_state.is_gameover():
-                                    board_list.append(board_state.board_2d)
-                                    legal_moves_9 = []
-                                    eight_move_games += 1
-
-                                else:
-                                    legal_moves_9 = board_state.get_valid_move_indexes()
-
-                                for l9 in legal_moves_9:
-                                    board_state = board.play_move(l2).play_move(l3).play_move(l4).play_move(l5).play_move(l6).play_move(l7).play_move(l8).play_move(l9)
-
-                                    board_list.append(board_state.board_2d)
-
-                                    if board_state.get_game_result() == 1: # only player one can win
-                                        nine_move_wins += 1
-
-                                    else:
-                                        nine_move_ties += 1
+# In[6]:
+print('\t \t mean \t \t std. dev')
+print('Ties: \t \t', '{:.3f} \t'.format(np.mean(ties)),  '{:.3f}'.format(np.std(ties)))
+print('X Wins: \t',  '{:.3f} \t'.format(np.mean(X_win)), '{:.3f}'.format(np.std(X_win)))
+print('O Wins: \t',  '{:.3f} \t'.format(np.mean(O_win)), '{:.3f}'.format(np.std(O_win)))
 
 
-
-
-
-# In[205]:
-print(len(board_list))
-
-print(five_move_games)
-print(six_move_games)
-print(seven_move_games)
-print(eight_move_games)
-print(nine_move_wins)
-print(nine_move_ties)
-
-# In[208]:
-# recursive function to build a list of all end of game board orientations (win or draw)
-def board_state_moves_and_play(board_state, board_list=None, number_moves=None, game_result=None):
+# In[7]:
+# Use a recursive function to build a list of all end of game board orientations (win or draw)
+# and this can be used to double check that our "random" results make sense
+def board_state_moves_and_result(board_state, board_list=None, number_moves=None, game_result=None):
     if board_list is None:
         board_list = []
 
@@ -339,37 +258,294 @@ def board_state_moves_and_play(board_state, board_list=None, number_moves=None, 
         legal_moves = board_state.get_valid_move_indexes()
 
         for l_m in legal_moves:
-            board_state_moves_and_play(board_state.play_move(l_m), board_list, number_moves, game_result)
+            board_state_moves_and_result(board_state.play_move(l_m), board_list, number_moves, game_result)
 
     return board_list, number_moves, game_result
 
+# In[8]:
 board = Board()
 
-boards, moves, results = board_state_moves_and_play(board)
+boards, moves, results = board_state_moves_and_result(board)
 
-df = pd.DataFrame()
+# In[9]:
+df_results = pd.DataFrame()
 
-df['Boards'] = boards
-df['# of Moves'] = moves
-df['Game Results'] = results
+df_results['Boards'] = boards
+df_results['# of Moves'] = moves
+df_results['Game Results'] = results
 
-# In[500]:
-print('Total Number of End States: \t', len(df))
+# In[10]:
+df_results_summary = pd.DataFrame()
+print('Total Number of End States: \t', len(df_results))
+print()
 
 total_check = 0
 for m in range(5,9):
-    games = np.count_nonzero(df['# of Moves'] == m)
-    total_check += games
-    print('{} Move Games: \t \t \t'.format(m), games)
+    games = np.count_nonzero(df_results['# of Moves'] == m)
+    df_results_summary['{} Moves'.format(m)] = [games]
 
 m = 9
-nine_game_wins = np.count_nonzero((df['# of Moves'] == 9) & (df['Game Results'] == +1))
-nine_game_ties = np.count_nonzero((df['# of Moves'] == 9) & (df['Game Results'] == 0))
+nine_move_wins = np.count_nonzero((df_results['# of Moves'] == 9) & (df_results['Game Results'] == +1))
+nine_move_ties = np.count_nonzero((df_results['# of Moves'] == 9) & (df_results['Game Results'] == 0))
 
-total_check += nine_game_wins + nine_game_ties
+df_results_summary['{} Moves'.format(m)] = nine_move_wins
+df_results_summary['Ties'] = nine_move_ties
 
-print('{} Move Games - Wins: \t \t'.format(m), nine_game_wins)
-print('{} Move Games - Ties: \t \t'.format(m), nine_game_ties)
-print(total_check)
+total_check += nine_move_wins + nine_move_ties
 
-# In[600]:
+
+print(df_results_summary.to_markdown(index=False))
+print(sum(sum(df_results_summary.values)))
+
+# In[11]:
+X_win_total = 0
+O_win_total = 0
+advance_percent = 1
+games_to_move_k = math.factorial(9)/math.factorial(9-5)
+for k in range(5,10):
+    games_won = df_results_summary['{} Moves'.format(k)].values[0]
+
+    win_percent_state = games_won / games_to_move_k
+    win_percent_cum = win_percent_state * advance_percent
+    advance_percent *= 1 - win_percent_state
+
+    print('Game states at move {}: \t \t \t'.format(k), int(games_to_move_k))
+    print('Game states won on move {}: \t \t'.format(k),games_won)
+    games_to_advance = games_to_move_k - games_won
+
+    if k == 9:
+        print('Game states that tie: \t \t \t', int(games_to_advance))
+    else:
+        print('Game states to advance: \t \t', int(games_to_advance))
+        games_to_move_k = games_to_advance * (9-k)
+
+    if k % 2 == 1:
+        X_win_total += win_percent_cum
+        color = [0.4, 0.4, 1]
+    else:
+        O_win_total += win_percent_cum
+        color = [1, 0.4, 0.4]
+
+    print('Fraction of Game States Won: \t \t', win_percent_state)
+    print('Cummulative Win Fraction: \t \t', win_percent_cum)
+    print('Fraction of games to advance: \t \t', advance_percent)
+
+    print()
+
+    plt.bar('{}'.format(k), win_percent_cum*100, color = color)
+
+plt.bar('Tie', advance_percent*100, color='gray')
+plt.bar('X Win', X_win_total*100, color='blue')
+plt.bar('O Win', O_win_total*100, color='red')
+
+print('Fraction of Games Won by X: \t \t', X_win_total)
+print('Fraction of Games Won by O: \t \t', O_win_total)
+print('Fraction of Games That Tie: \t \t', advance_percent)
+
+# In[12]:
+'''
+Agenda
+        Write up summary of results
+        Plot the most frequent win positions
+'''
+
+ax, fig = axes(rows=8, columns=2)
+
+games_to_move_k = math.factorial(9)/math.factorial(9-5)
+advance_percent = 1
+
+_x_win_pos_ = np.zeros((3,3))
+_o_win_pos_ = np.zeros((3,3))
+
+_x_loss_pos_ = np.zeros((3,3))
+_o_loss_pos_ = np.zeros((3,3))
+
+for m in range(5,11):
+    board_ = df_results[df_results['# of Moves'] == m]['Boards']
+
+    if m == 9:
+        board_ = df_results[(df_results['# of Moves'] == m) & (df_results['Game Results'] == +1)]['Boards']
+
+    elif m == 10:
+        board_ = df_results[(df_results['# of Moves'] == 9) & (df_results['Game Results'] == 0)]['Boards']
+
+    _x_pos_ = np.zeros((3,3))
+    _o_pos_ = np.zeros((3,3))
+    ___pos_ = np.zeros((3,3))
+    for b in board_:
+        _x_pos_ += np.where(b == 1, b, 0)
+        _o_pos_ += np.where(b == -1, b, 0)
+        ___pos_ += np.where(b == 0, 1, 0)
+
+    # normalize
+    _x_pos_ *= 100/sum(sum(_x_pos_))
+    _o_pos_ *= 100/sum(sum(_o_pos_))
+
+    if m < 9:
+        ___pos_ /= sum(sum(___pos_))
+        ___pos_ *= 100
+
+    if m <= 9:
+        if m % 2 == 1:
+            _x_win_pos_ += _x_pos_
+            _o_loss_pos_ += _o_pos_
+        else:
+            _o_win_pos_ += _o_pos_
+            _x_loss_pos_ += _x_pos_
+
+        games_won = df_results_summary['{} Moves'.format(m)].values[0]
+        win_percent_state = games_won / games_to_move_k
+        win_percent_cum = win_percent_state * advance_percent
+        advance_percent *= 1 - win_percent_state
+        games_to_move_k = games_to_advance * (9-m)
+
+    ax1 = ax[2*(m-5)+1]
+    ax2 = ax[2*(m-5)+2]
+    _1 = ax1.pcolormesh(_x_pos_, cmap='Blues', vmin=0, vmax=20)
+    _2 = ax2.pcolormesh(_o_pos_, cmap='Reds',  vmin=0, vmax=20)
+    # fig.colorbar(_)
+
+    cbar1 = fig.colorbar(_1, ax=ax1)
+    cbar2 = fig.colorbar(_2, ax=ax2)
+
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+
+
+# normalize
+_x_win_pos_  *= 100/sum(sum(_x_win_pos_))
+_x_loss_pos_ *= 100/sum(sum(_x_loss_pos_))
+_o_win_pos_  *= 100/sum(sum(_o_win_pos_))
+_o_loss_pos_ *= 100/sum(sum(_o_loss_pos_))
+
+ax1 = ax[13]
+ax2 = ax[14]
+_1 = ax1.pcolormesh(_x_win_pos_, cmap='Blues', vmin=0)
+_2 = ax2.pcolormesh(_o_win_pos_, cmap='Reds',  vmin=0)
+
+cbar1 = fig.colorbar(_1, ax=ax1)
+cbar2 = fig.colorbar(_2, ax=ax2)
+
+ax1.set_xticks([])
+ax1.set_yticks([])
+
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+
+ax1 = ax[15]
+ax2 = ax[16]
+_1 = ax1.pcolormesh(_x_loss_pos_, cmap='Blues', vmin=0)
+_2 = ax2.pcolormesh(_o_loss_pos_, cmap='Reds',  vmin=0)
+
+cbar1 = fig.colorbar(_1, ax=ax1)
+cbar2 = fig.colorbar(_2, ax=ax2)
+
+ax1.set_xticks([])
+ax1.set_yticks([])
+
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+ax[1].set_title('X Position Frequency')
+ax[2].set_title('O Position Frequency')
+
+ax[1].set_ylabel('5 Move Games \n(X Wins)')
+ax[3].set_ylabel('6 Move Games \n(O Wins)')
+ax[5].set_ylabel('7 Move Games \n(X Wins)')
+ax[7].set_ylabel('8 Move Games \n(O Wins)')
+ax[9].set_ylabel('9 Move Games \n(X Wins)')
+ax[11].set_ylabel('9 Move Games \n(Draw)')
+ax[13].set_ylabel('Overall Winning Positions')
+ax[15].set_ylabel('Overall Losing Positions')
+
+fig.tight_layout()
+
+# In[20]:
+'''
+Summary of Results
+
+1. The programmed game works as expected
+2. The results of both players choosing random moves are in agreement with the closed form statistics of the possible board orientations
+3. A recursive function was written and used to identify all possible board states
+4. The results from the recursive investigation are again in agreement with the closed form mathematics
+5. A bar chart illustrates the statistics of all possible game results
+6. 2-dimensional colormaps illustrate the most frequently played positions for games won and lost
+    a. These results suggest that there is a hierarchy of the positional value
+        1. The most valuable board position is the middle square
+        2. The second most valuable positions are the corner squares
+        3. The least valuable squares are the middle edge squares
+    b. This is consistent with what we know about the game and from a superficial glance at combinatorics
+        1. There are 8 possible winning orientations (see top)
+        2. 4 of them involve the middle square
+        3. each corner square can be used in 3 winning orientations
+        4. each middle edge square can only be used in 2 winning orientations
+    c. These results seem to suggest a possible game strategy that can be outlined in 3 lines
+        1. choose the middle square if available
+        2. choose a corner square if available
+        3. else choose randomly (all that remain are the middle edge squares)
+'''
+
+# In[21]:
+def tier_strategy(board):
+    # Simple, non-random, tiered square strategy
+    available_moves = board.get_valid_move_indexes()
+    middle_square = 4
+    corner_squares = [0, 2, 6, 8]
+    avail_corners = [x for x in corner_squares if x in available_moves]
+
+    if 4 in available_moves:
+        move_ind = 4
+    elif avail_corners:
+        move_ind = random.choice(avail_corners)
+    else:
+        move_ind = random.choice(board.get_valid_move_indexes())
+
+    return move_ind
+
+# In[22]:
+number_games = 1000
+game_results = np.zeros(number_games)
+game_moves   = np.zeros(number_games)
+for i in range (number_games):
+    board = Board()
+    while not board.is_gameover():
+        if board.get_turn() == 1: # player one
+            move_ind = tier_strategy(board)
+        else:
+            move_ind = random.choice(board.get_valid_move_indexes())
+
+        board = board.play_move(move_ind)
+
+    game_results[i] = board.get_game_result()
+    game_moves[i]   = np.count_nonzero(board.board)
+
+# In[23]:
+print(np.count_nonzero(game_results == 1) / len(game_results))
+print(np.count_nonzero(game_results == 0) / len(game_results))
+print(np.count_nonzero(game_results == -1) / len(game_results))
+
+
+# In[24]:
+number_games = 1000
+game_results = np.zeros(number_games)
+game_moves   = np.zeros(number_games)
+for i in range (number_games):
+    board = Board()
+    while not board.is_gameover():
+        if board.get_turn() == -1: # player two
+            move_ind = tier_strategy(board)
+        else:
+            move_ind = random.choice(board.get_valid_move_indexes())
+
+        board = board.play_move(move_ind)
+
+    game_results[i] = board.get_game_result()
+    game_moves[i]   = np.count_nonzero(board.board)
+
+print(np.count_nonzero(game_results == -1) / len(game_results))
+print(np.count_nonzero(game_results == 0) / len(game_results))
+print(np.count_nonzero(game_results == 1) / len(game_results))
